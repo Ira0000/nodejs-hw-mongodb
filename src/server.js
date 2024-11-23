d1ce7666bac86215ed7af9bd1fa13039207fca71
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-11-23 18:10:42 +0100

    create node.js project and add dependencies and add config step 1, 2, 3

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = Number(process.env.PORT) || 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
