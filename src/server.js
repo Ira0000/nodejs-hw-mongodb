a1f1eeb162af9087a64118cbb63c4556d83e9196
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-11-23 18:48:47 +0100

    create rote GET /contacts/ and GET /contacts/:contactId

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import dotenv from 'dotenv';
+import { getAllContacts, getContactById } from './services/contacts';
 
 dotenv.config();
 const PORT = Number(process.env.PORT) || 3000;
@@ -27,6 +28,29 @@ export const setupServer = () => {
     res.status(500).json({
       message: 'Something went wrong',
       error: err.message,
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts;
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contactId) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+    res.status(200).json({
+      data: contact,
+      message: `Successfully found contact with id ${contactId}`,
     });
   });
 
