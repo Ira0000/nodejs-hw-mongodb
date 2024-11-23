1f0fba8df3f65a3cf5e1505467d4164a2c7c4406
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-11-23 19:23:35 +0100

    change order of middlewarre in services.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const PORT = Number(process.env.PORT) || 3000;
 export const setupServer = () => {
   const app = express();
 
+  app.use(express.json());
+  app.use(cors());
   app.use(
     pino({
       transport: {
@@ -17,19 +19,6 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(cors());
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts;
@@ -51,6 +40,19 @@ export const setupServer = () => {
     res.status(200).json({
       data: contact,
       message: `Successfully found contact with id ${contactId}`,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
     });
   });
 
